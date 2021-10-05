1fbfc8bcaf523dc848d2cea9c9d1df49f6b85219
Siddharth Khuntwal <achillesid34@gmail.com>
2021-10-05 20:37:26 +0530

    Contact Us page added in router component

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../App";
 import Header from "./Header/Header";
 import Resources from "./Resources/Resources";
+import Contact from "./ContactUs/Contact";
 
 const Router = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Router = () => (
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/resources" component={Resources} />
+      <Route exact path="/contact" component={Contact} />
     </Switch>
   </BrowserRouter>
 );
