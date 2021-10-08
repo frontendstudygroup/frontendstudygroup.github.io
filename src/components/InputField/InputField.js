fc4f4e611418123d89f5fc55e54342eb507aa432
Akshit <akshitdandyan@outlook.com>
2021-10-08 10:14:10 +0530

    Create separate Input Component

---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,18 @@
+import "./InputField.css";
+
+const InputField = ({ label, placeholder, onChange, value, error, type }) => {
+    return (
+        <div className="inputfield_wrapper">
+            <label>{ label ? label : "" }</label>
+            <input type={ type ? type : "text" } id="email" placeholder={ placeholder }
+                value={ value && value }
+                onChange={onChange && onChange}
+            />
+            <p className="formContentError" >
+                { error && type === "email" ? "Please enter a valid email" : error ? "This field is required" : ""}
+            </p>
+        </div>
+    )
+}
+
+export default InputField
