ecc0c6ffa89f1adb2f2624f263ac53798ca4a452
Ingrid04 <ingridfmelgar@gmail.com>
2021-10-13 15:51:35 -0400

    Test file deleted

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Supported by Women Who Code link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Supported by Women Who Code/i);
-  expect(linkElement).toBeInTheDocument();
-});
