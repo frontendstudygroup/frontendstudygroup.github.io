420a43004cbac1c0626d5e573c205f99fb21cd06
Ingrid04 <ingridfmelgar@gmail.com>
2021-10-12 23:33:31 -0400

    Main component deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import '../src/assets/css/App.css';
 import HomePage from './components/HomePage/HomePage';
-// import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
       <HomePage />
     </div>
   );
