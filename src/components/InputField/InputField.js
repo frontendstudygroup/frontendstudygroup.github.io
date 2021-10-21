6a72a56e7af7e989938ae3934cfc20f187f42983
awantika10 <awantikanigam101220@gmail.com>
2021-10-21 15:27:25 +0530

    Added dockerfile and Nginx.conf files

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,18 +1,25 @@
 import "./InputField.css";
-
+import React from "react";
 const InputField = ({ label, placeholder, onChange, value, error, type }) => {
-    return (
-        <div className="inputfield_wrapper">
-            <label>{ label ? label : "" }</label>
-            <input type={ type ? type : "text" } id="email" placeholder={ placeholder }
-                value={ value && value }
-                onChange={onChange && onChange}
-            />
-            <p className="formContentError" >
-                { error && type === "email" ? "Please enter a valid email" : error ? "This field is required" : ""}
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="inputfield_wrapper">
+      <label>{label ? label : ""}</label>
+      <input
+        type={type ? type : "text"}
+        id="email"
+        placeholder={placeholder}
+        value={value && value}
+        onChange={onChange && onChange}
+      />
+      <p className="formContentError">
+        {error && type === "email"
+          ? "Please enter a valid email"
+          : error
+          ? "This field is required"
+          : ""}
+      </p>
+    </div>
+  );
+};
 
-export default InputField
+export default InputField;
