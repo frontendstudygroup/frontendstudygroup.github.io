377450d720f27dabbf65e90a4f79690850c1eb5f
Priya Ramachanriya Surendranath <priyami2010@gmail.com>
2021-11-02 18:34:15 -0400

    More info link validate

--- src/components/Resources/Resources.test.js
+++ src/components/Resources/Resources.test.js
@@ -1,9 +1,10 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { MemoryRouter, Link } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import SingleResource from '../SingleResource/SingleResource';
 import Resources from "./Resources";
 import Toggle from "../Toggle";
+import { BrowserRouter, Router, Link } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import data from '../data/data.json';
 
 // test.afterEach(cleanup)
 
@@ -27,16 +28,30 @@ test("resource body", () => {
     const resourceBody = screen.getByTestId("body");
     expect(resourceBody).toBeInTheDocument();
 });
-test("link working", () => {
-    const history = createMemoryHistory();
-    history.push = jest.fn();
+test(" More info Link validation", async () => {
+    //check the `More info` link 12 times rendered on /resouces page 
+    //check when click moreinfo link routes to corresponding page /resources/resource.id 
+    //check each page has resource.name using getByText on the screen
 
-    const { getByText } = render(
-        <MemoryRouter history={history}>
-          <Link to={`/resources/${resource.id}`}>More info</Link>
-        </MemoryRouter>
-      );
+    let history = createMemoryHistory({ initialEntries: ["/resources"] });
 
-      fireEvent.click(getByText('More info'));
-      expect(history.push).toHaveBeenCalledWith(`/resources/${resource.id}`);
+    render(<BrowserRouter><Resources ResourcesList={data} /></BrowserRouter>);
+    let moreinfoButtons = screen.getAllByText("More info");
+    for (let resource of data) 
+    {
+        
+        <Router history={history}>
+            <Link to={{ pathname: `/resources/${resource.id}` }}>More info</Link>
+        </Router>
+
+        expect(moreinfoButtons).toHaveLength(12)
+        
+        fireEvent.click(moreinfoButtons[`${resource.id-1}`]);
+
+        expect(location.pathname).toEqual(`/resources/${resource.id}`);
+
+        await expect(screen.getByText(`${resource.name}`)).toBeInTheDocument();
+
+    }   
+
 });
