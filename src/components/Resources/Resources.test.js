02d39a72c224159a76424b22acd1b415ef160554
Priya Ramachanriya Surendranath <priyami2010@gmail.com>
2021-11-10 23:01:27 -0500

    Test Resources component

--- src/components/Resources/Resources.test.js
+++ src/components/Resources/Resources.test.js
@@ -1,57 +1,56 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import SingleResource from '../SingleResource/SingleResource';
-import Resources from "./Resources";
+import Resources from "../Resources/Resources";
 import Toggle from "../Toggle";
 import { BrowserRouter, Router, Link } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import data from '../data/data.json';
 
-// test.afterEach(cleanup)
-
-describe("Resources Component", () => {
-    it('rendered toggle', () => {
+describe("Resources", () => {
+    
+    test("renders Resources Component", () => {
+        const {getByText} = render(<BrowserRouter><Resources/></BrowserRouter>);
+        expect(screen.getByText("Open Source Programs")).toBeInTheDocument;
+    })
+   test('rendered toggle', () => {
         const { getByTestId } = render(<Toggle />);
         const toggle = getByTestId("toggle");
         expect(toggle).toBeTruthy();
-    });
+    })
+    test("resource name", () => {
+        render(<div data-testid="name" />);
+        const resourceName = screen.getByTestId("name");
+        expect(resourceName).toBeInTheDocument();
+    })
+    test("resource body", () => {
+        render(<div data-testid="body" />);
+        const resourceBody = screen.getByTestId("body");
+        expect(resourceBody).toBeInTheDocument();
+    })
+    test(" More info Link validation", async () => {
+        //check the `More info` link 12 times rendered on /resouces page 
+        //check when click moreinfo link routes to corresponding page /resources/resource.id 
+        //check each page has resource.name using getByText on the screen
+    
+        let history = createMemoryHistory({ initialEntries: ["/resources"] });
+    
+        render(<BrowserRouter><Resources ResourcesList={data} /></BrowserRouter>);
+        let moreinfoButtons = screen.getAllByText("More info");
+        for (let resource of data) 
+        {
+            
+            <Router history={history}>
+                <Link to={{ pathname: `/resources/${resource.id}` }}>More info</Link>
+            </Router>
+    
+            expect(moreinfoButtons).toHaveLength(12)
+            
+            fireEvent.click(moreinfoButtons[`${resource.id-1}`]);
+    
+            expect(location.pathname).toEqual(`/resources/${resource.id}`);
+    
+            await expect(screen.getByText(`${resource.name}`)).toBeInTheDocument();
+    
+        }   
+    })
+    screen.debug();
 })
-
-test("resource name", () => {
-    render(<div data-testid="name" />);
-    const resourceName = screen.getByTestId("name");
-    expect(resourceName).toBeInTheDocument();
-});
-// screen.debug();
-
-test("resource body", () => {
-    render(<div data-testid="body" />);
-    const resourceBody = screen.getByTestId("body");
-    expect(resourceBody).toBeInTheDocument();
-});
-test(" More info Link validation", async () => {
-    //check the `More info` link 12 times rendered on /resouces page 
-    //check when click moreinfo link routes to corresponding page /resources/resource.id 
-    //check each page has resource.name using getByText on the screen
-
-    let history = createMemoryHistory({ initialEntries: ["/resources"] });
-
-    render(<BrowserRouter><Resources ResourcesList={data} /></BrowserRouter>);
-    let moreinfoButtons = screen.getAllByText("More info");
-    for (let resource of data) 
-    {
-        
-        <Router history={history}>
-            <Link to={{ pathname: `/resources/${resource.id}` }}>More info</Link>
-        </Router>
-
-        expect(moreinfoButtons).toHaveLength(12)
-        
-        fireEvent.click(moreinfoButtons[`${resource.id-1}`]);
-
-        expect(location.pathname).toEqual(`/resources/${resource.id}`);
-
-        await expect(screen.getByText(`${resource.name}`)).toBeInTheDocument();
-
-    }   
-
-});
